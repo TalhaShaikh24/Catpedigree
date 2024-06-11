1de91f7c2c813e8f1a349bfc157958d8e649ae2c
TalhaShaikh24 <87939410+TalhaShaikh24@users.noreply.github.com>
2024-06-11 14:03:05 +0500

    Changes

--- WebApp/wwwroot/webAssets/PagesJS/SingleListing.js
+++ WebApp/wwwroot/webAssets/PagesJS/SingleListing.js
@@ -1,95 +1,84 @@
-﻿
-$("#choosepackage").click(function () {
+﻿$(".btnChoosePkg").click(function () {
+    var packageId = $(this).data("id");
 
-    
-    if ($('input[name="showpromotionpackage"]:checked').length===0) {
-        Swal.fire({
-            title: "Error",
-            text: "Select the package ",
-            icon: "error"
-        })
-        return;
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'This action will detuct 1 listing form the package. Continue?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, select it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            postRequest('/Listing/SelectPackageListingShowValidation/' + packageId, null, function (res) {
 
-    }
+                if (res.status == 200) {
 
-   
-    else {
 
-        var packageId = $('input[name="showpromotionpackage"]:checked').val();
-        debugger;
-        postRequest('/Listing/SelectPackageListingShowValidation/' + packageId, null, function (res) {
-
-            if (res.status == 200) {
-
-              
                     window.location.reload();
 
-                
-            }
-            if (res.status == 304) {
 
-                Swal.fire({
-                    title: "Error",
-                    text: res.responseMsg,
-                    icon: "error"
-                })
-            }
-            if (res.status == 305) {
+                }
+                if (res.status == 304) {
 
-                Swal.fire({
-                    title: "Error",
-                    text: res.responseMsg,
-                    icon: "error"
-                })
-            }
-            if (res.status == 401) {
+                    Swal.fire({
+                        title: "Error",
+                        text: res.responseMsg,
+                        icon: "error"
+                    })
+                }
+                if (res.status == 305) {
 
-                Swal.fire({
-                    title: "Error",
-                    text: res.responseMsg,
-                    icon: "error"
-                })
-            }
-            if (res.status == 403) {
+                    Swal.fire({
+                        title: "Error",
+                        text: res.responseMsg,
+                        icon: "error"
+                    })
+                }
+                if (res.status == 401) {
 
-                Swal.fire(res.responseMsg, {
-                    icon: "error",
-                    title: "Error"
-                });
-            }
-            if (res.status == 320) {
+                    Swal.fire({
+                        title: "Error",
+                        text: res.responseMsg,
+                        icon: "error"
+                    })
+                }
+                if (res.status == 403) {
 
-                Swal.fire({
-                    title: "Error",
-                    text: res.responseMsg,
-                    icon: "error"
-                })
-            }
-            if (res.status == 500) {
+                    Swal.fire(res.responseMsg, {
+                        icon: "error",
+                        title: "Error"
+                    });
+                }
+                if (res.status == 320) {
 
-                Swal.fire({
-                    title: "Error",
-                    text: res.responseMsg,
-                    icon: "error"
-                })
-            }
-            if (res.status == 600) {
+                    Swal.fire({
+                        title: "Error",
+                        text: res.responseMsg,
+                        icon: "error"
+                    })
+                }
+                if (res.status == 500) {
 
-                Swal.fire({
-                    title: "Warning",
-                    text: res.responseMsg,
-                    icon: "warning"
-                })
+                    Swal.fire({
+                        title: "Error",
+                        text: res.responseMsg,
+                        icon: "error"
+                    })
+                }
+                if (res.status == 600) {
 
-            }
-        });
+                    Swal.fire({
+                        title: "Warning",
+                        text: res.responseMsg,
+                        icon: "warning"
+                    })
 
-
-    }
-
-
-
-
+                }
+            });
+        }
+    });
 });
 
 
