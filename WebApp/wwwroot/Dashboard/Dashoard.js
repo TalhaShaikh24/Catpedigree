434f649d482a6429dada04268122a384324bdefa
sheerazkhilji <sheeraz.kxoft@gmail.com>
2024-10-08 05:59:59 -0700

    dashboard changes

--- WebApp/wwwroot/Dashboard/Dashoard.js
+++ WebApp/wwwroot/Dashboard/Dashoard.js
@@ -11,10 +11,10 @@ function GetAllDashbaord() {
     postRequest('/Dashboard/GetAllDashboard', null, function (res) {
 
         if (res.status == 200) {
+            debugger
+            if (res.data.jsonObject != null) {
 
-            if (res.data != null) {
-
-                var IndexZero = JSON.parse(res.data)[0];
+                var IndexZero = JSON.parse(res.data.jsonObject)[0];
                 $("#TotalListingText").text(IndexZero.TotalListing);
                 $("#ActiveListingText").text(IndexZero.ActiveListing);
                 $("#ExpiredListingText").text(IndexZero.ExpiredListing);
@@ -23,19 +23,71 @@ function GetAllDashbaord() {
                 $("#RejectedListingText").text(IndexZero.TotalRejectedListing);
                 $("#ActivePlanText").text(IndexZero.TotalActivePlan);
                 $("#ExpiredPlanText").text(IndexZero.TotalExpiredPlan);
+
+                $("#TotalAdvertisementPackages").text(IndexZero.TotalAdvertisementPackages);
+                $("#TotalUtlizedAds").text(IndexZero.TotalUtlizedAds);
+                $("#TotalAdsApproved").text(IndexZero.TotalAdsApproved);
+
+                $("#TotalAdsReject").text(IndexZero.TotalAdsReject);
+                
+                $("#TotalAdsPending").text(IndexZero.TotalAdsPending);
+
+
                 $("#AppendMyPackages").empty();
 
-                for (let i = 1; i <= JSON.parse(res.data).length - 1; i++) {
+                for (let i = 1; i <= JSON.parse(res.data.jsonObject).length - 1; i++) {
                     debugger
                     $("#AppendMyPackages").append(`
                         <tr>
-                                <td>${JSON.parse(res.data)[i].Name}</td>
-                                <td>${JSON.parse(res.data)[i].AllowedListings == 999 ? "UNLIMITED" : JSON.parse(res.data)[i].AllowedListings}</td>
-                                <td>${JSON.parse(res.data)[i].RemainingListings == 999 ? "UNLIMITED" : JSON.parse(res.data)[i].RemainingListings}</td>
+                                <td>${JSON.parse(res.data.jsonObject)[i].Name}</td>
+                                <td>${JSON.parse(res.data.jsonObject)[i].AllowedListings == 999 ? "UNLIMITED" : JSON.parse(res.data.jsonObject)[i].AllowedListings}</td>
+                                <td>${JSON.parse(res.data.jsonObject)[i].RemainingListings == 999 ? "UNLIMITED" : JSON.parse(res.data.jsonObject)[i].RemainingListings}</td>
                         </tr>`);
                 }
 
                 $('#TableMyPackages').DataTable();
+               
+                for (let i = 1; i <= res.data.assignPromotionPackage.length - 1; i++) {
+                 
+                    $("#AppendMyPromotionPackages").append(`
+                        <tr>
+                                <td>${res.data.assignPromotionPackage[i].promotionPackageName}</td>
+                                <td>${moment(res.data.assignPromotionPackage[i].subscriptionDate).format("DD - MMMM - YYYY") }</td>
+                               <td>${moment(res.data.assignPromotionPackage[i].expiryDate).format("DD - MMMM - YYYY") }</td>
+
+                               </tr>`);
+                }
+               
+                $('#TableMyPromotionPackages').DataTable();
+
+
+                debugger;
+                $.each(res.data.advertisementPackage, function (i, v) {
+                    debugger;
+                    $("#AppendApprovalListing").append(`
+                                                <tr>
+
+                                                   <td>${v.userAdvertisementPackageID}</td>
+                                                   <td>${v.advertisementPackageName}</td>
+                                                   <td>${v.advertisementPackageCost}</td>
+                                                   <td>${v.advertisementPackageType}</td>
+                                                   <td>${v.numberOfAdvertisement}</td>
+                                                   <td>${v.status}</td>
+                                                 
+                                                   
+                                                   <td>${moment(v.createdOn).format("DD - MMMM - YYYY") }</td>
+                                                   <td> <img src="${baseApiUrl + v.filePath}" style="
+
+    height: 51px;
+    width: 51px;
+" />   </td>
+                                                   
+
+                                                </tr>`);
+
+                });
+
+                $('#TableApprovalListing').DataTable();
 
             }
         }
